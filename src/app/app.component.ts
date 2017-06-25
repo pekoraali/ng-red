018997fa7657416cd78aff9ff5bb56452f559a3c
pekoraali <pekoraali@protonmail.com>
2017-06-25 21:50:02 +0300

    Fixed data.json position. Changed md-toolbar. Added md-tabs and md-card iteration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import {Component} from "@angular/core";
-
+import {Http} from "@angular/http";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app works!';
+  spaceScreens: Array<any>;
+
+  constructor(private http: Http) {
+    this.http.get('./assets/data.json')
+        .map(response => response.json().screenshots)
+        .subscribe(res => this.spaceScreens = res);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+//       ^^^^^^^^^^^^^^^^^^^^^^^
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
-
 import "@angular/animations";
 import {MaterialModule} from "@angular/material";
 import {AppComponent} from "./app.component";
@@ -15,6 +16,8 @@ import "hammerjs";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+//       ^^^^^^^^^^^^^^^^^^^^^^^
     FormsModule,
     HttpModule,
     MaterialModule
